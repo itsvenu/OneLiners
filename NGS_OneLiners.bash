22809d6c1c8e3daa378e57b1c048668e914a3360
venu <thatikonda92@gmail.com>
2015-12-09 10:54:41 +0530

    Update NGS_OneLiners.bash

--- NGS_OneLiners.bash
+++ NGS_OneLiners.bash
@@ -1,6 +1,17 @@
 
 #Generate a vcf.gz along with its tabix indexed file
+#To use with vcftools
 
 bgzip -c file.vcf > file.vcf.gz
 tabix -p vcf file.vcf.gz
+
+#Compare two VCF files and list out variants present on file1
+
+vcf-isec -c file1.vcf.gz file2.vcf.gz file3.vcf.gz | bgzip -c > file1_only.vcf.gz
+
+# Common variants in 2 VCF files
+
+vcf-isec -n +2 file1.vcf.gz file2.vcf.gz | bgzip -c > CommonVar.vcf.gz
+
+
 
